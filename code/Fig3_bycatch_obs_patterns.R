84d7f5bea216e1dea720b675ea0f73ee134e890f
tinafang-1207 <yutianfang@ucsb.edu>
2023-01-03 16:35:44 -0800

    make figures for cpue

--- code/Fig3_bycatch_obs_patterns.R
+++ code/Fig3_bycatch_obs_patterns.R
@@ -11,9 +11,16 @@ library(tidyverse)
 
 ### Read in data ###
 ####################
+
+#### total merge: extracted depth ####
+
 total_merge <- read.csv("data/confidential/processed/fig3_merge_obs_and_trip_data.csv")
 
 total_merge_all <- read.csv("data/confidential/processed/fig3_merge_obs_and_trip_data_all.csv")
+
+#### Sensitive Species ####
+#### Only find Harbor seal and California Sea lion from 1980's data ####
+#### What about Common murre and Harbor porpoise?####
 
 #####################################################################
 ### Panel A (1) - Distance to shore and BPUE of sensitive species ###
@@ -332,3 +339,151 @@ g4_jd <- ggplot(data = mammal_bycatch_jd, aes(x=julian_day, y = proportion_seal,
 
 g4_jd
 
+################################################
+### Panel B (1) - Distance to shore and CPUE ###
+################################################
+
+
+total_merge_dist_cpue <- total_merge %>%
+  mutate(dist_catg = ifelse(dist_km <= 10,
+                            cut(dist_km, breaks = seq(0,10.5,0.5), label = seq(0, 10, 0.5), right = F) %>% as.character() %>% as.numeric(),
+                            cut(dist_km, breaks = seq(10,80,5), label = seq(10, 75, 5), right = F) %>% as.character() %>% as.numeric()))
+
+number_sets_dist_cpue <- total_merge_dist_cpue %>%
+  group_by(data_source, dist_catg) %>%
+  summarize(number_sets = n_distinct(set_id)) %>%
+  unite (unique_id, c(data_source, dist_catg), sep = "-", remove = FALSE)
+
+number_effort_dist_cpue <- total_merge_dist_cpue %>%
+  filter(!is.na(net_length_fa)) %>%
+  filter(!is.na(soak_hr)) %>%
+  select(set_id, net_length_fa, soak_hr, data_source, dist_catg) %>%
+  # turn units from hrs*fa into day*miles
+  mutate(effort = soak_hr*net_length_fa * (1/(880*24))) %>%
+  filter(!duplicated(set_id)) %>%
+  group_by(data_source, dist_catg) %>%
+  summarize(number_efforts = sum(effort)) %>%
+  unite (unique_id, c(data_source, dist_catg), sep = "-", remove = TRUE)
+
+number_retained_dist_cpue <- total_merge_dist_cpue %>%
+  # filter out NA n_retained species
+  filter(!is.na(n_retained)) %>%
+  group_by(data_source, dist_catg) %>%
+  summarize(number_retained = sum(n_retained)) %>%
+  unite (unique_id, c(data_source, dist_catg), sep = "-", remove = TRUE)
+
+dist_cpue <-  merge(number_sets_dist_cpue, number_effort_dist_cpue, by = "unique_id", all.x = TRUE) %>%
+  merge(number_retained_dist_cpue, by = "unique_id", all.x = TRUE) %>%
+  mutate(cpue = number_retained/number_efforts) %>%
+  # filter out NA cpue value
+  filter(!is.na(cpue))
+
+base_theme <- theme(axis.text=element_text(size=6),
+                    axis.text.y = element_text(angle = 90, hjust = 0.5),
+                    axis.title=element_text(size=7),
+                    legend.text=element_text(size=6),
+                    legend.title=element_text(size=7),
+                    plot.tag =element_text(size=8),
+                    plot.title=element_blank(),
+                    # Gridlines
+                    panel.grid.major = element_blank(),
+                    panel.grid.minor = element_blank(),
+                    panel.background = element_blank(),
+                    axis.line = element_line(colour = "black"))
+
+g_cpue_dist <-  ggplot(data = dist_cpue, aes(x=dist_catg, y = cpue, fill = data_source, size = number_sets)) +
+  geom_point(pch = 21) +
+  lims(x = c(0, 20), y = c(0, 500)) +
+  theme_bw() + base_theme
+
+g_cpue_dist
+
+
+##########################################
+### Panel B (2) - haul depth and CPUE ###
+#########################################
+
+total_merge_depth_cpue <- total_merge %>%
+  mutate(depth_catg = ifelse(haul_depth_fa <= 20,
+                            cut(haul_depth_fa, breaks = seq(0,22,2), label = seq(0, 20, 2), right = F) %>% as.character() %>% as.numeric(),
+                            cut(haul_depth_fa, breaks = seq(20,460,40), label = seq(20, 420, 40), right = F) %>% as.character() %>% as.numeric()))
+
+number_sets_depth_cpue <- total_merge_depth_cpue %>%
+  filter(!is.na(haul_depth_fa)) %>%
+  group_by(data_source, depth_catg) %>%
+  summarize(number_sets = n_distinct(set_id)) %>%
+  unite (unique_id, c(data_source, depth_catg), sep = "-", remove = FALSE)
+
+number_effort_depth_cpue <- total_merge_depth_cpue %>%
+  filter(!is.na(net_length_fa)) %>%
+  filter(!is.na(soak_hr)) %>%
+  select(set_id, net_length_fa, soak_hr, data_source, depth_catg) %>%
+  # turn units from hrs*fa into day*miles
+  mutate(effort = soak_hr*net_length_fa * (1/(880*24))) %>%
+  filter(!duplicated(set_id)) %>%
+  group_by(data_source, depth_catg) %>%
+  summarize(number_efforts = sum(effort)) %>%
+  unite (unique_id, c(data_source, depth_catg), sep = "-", remove = TRUE)
+
+number_retained_depth_cpue <- total_merge_depth_cpue %>%
+  # filter out NA n_retained species
+  filter(!is.na(n_retained)) %>%
+  group_by(data_source, depth_catg) %>%
+  summarize(number_retained = sum(n_retained)) %>%
+  unite (unique_id, c(data_source, depth_catg), sep = "-", remove = TRUE)
+
+depth_cpue <-  merge(number_sets_depth_cpue, number_effort_depth_cpue, by = "unique_id", all.x = TRUE) %>%
+  merge(number_retained_depth_cpue, by = "unique_id", all.x = TRUE) %>%
+  mutate(cpue = number_retained/number_efforts) %>%
+  filter(!is.na(cpue))
+
+
+g_cpue_depth <-  ggplot(data = depth_cpue, aes(x=depth_catg, y = cpue, fill = data_source, size = number_sets)) +
+  geom_point(pch = 21) +
+  lims(x = c(0, 25), y = c(0, 250)) +
+  theme_bw() + base_theme
+
+g_cpue_depth
+
+############################################
+### Panel B (3) - Haul latitude and CPUE ###
+############################################
+
+total_merge_latitude_cpue <- total_merge %>%
+  mutate(lat_catg = cut(haul_lat_dd, breaks = seq(32.4, 35.7,0.2), 
+                        label = seq(32.4, 35.5, 0.2), right = F) %>% as.character() %>% as.numeric())
+
+number_sets_lat_cpue <- total_merge_latitude_cpue %>%
+  group_by(data_source, lat_catg) %>%
+  summarize(number_sets = n_distinct(set_id)) %>%
+  unite (unique_id, c(data_source, lat_catg), sep = "-", remove = FALSE)
+
+number_effort_lat_cpue <- total_merge_latitude_cpue %>%
+  filter(!is.na(net_length_fa)) %>%
+  filter(!is.na(soak_hr)) %>%
+  select(set_id, net_length_fa, soak_hr, data_source, lat_catg) %>%
+  # turn units from hrs*fa into day*miles
+  mutate(effort = soak_hr*net_length_fa * (1/(880*24))) %>%
+  filter(!duplicated(set_id)) %>%
+  group_by(data_source, lat_catg) %>%
+  summarize(number_efforts = sum(effort)) %>%
+  unite (unique_id, c(data_source, lat_catg), sep = "-", remove = TRUE)
+
+number_retained_lat_cpue <- total_merge_latitude_cpue %>%
+  # filter out NA n_retained species
+  filter(!is.na(n_retained)) %>%
+  group_by(data_source, lat_catg) %>%
+  summarize(number_retained = sum(n_retained)) %>%
+  unite (unique_id, c(data_source, lat_catg), sep = "-", remove = TRUE)
+
+lat_cpue <-  merge(number_sets_lat_cpue, number_effort_lat_cpue, by = "unique_id", all.x = TRUE) %>%
+  merge(number_retained_lat_cpue, by = "unique_id", all.x = TRUE) %>%
+  mutate(cpue = number_retained/number_efforts) %>%
+  filter(!is.na(cpue))
+
+g_cpue_lat <-  ggplot(data = lat_cpue, aes(x=lat_catg, y = cpue, fill = data_source, size = number_sets)) +
+  geom_point(pch = 21) +
+  #lims(x = c(0, 25), y = c(0, 250)) +
+  theme_bw() + base_theme
+
+g_cpue_lat
